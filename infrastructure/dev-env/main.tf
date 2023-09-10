d9f97865cef2c86f4964fcd4ed79ef22b40750d0
Gregory Carroll <gregscarroll@gmail.com>
2023-09-10 17:35:04 -0600

    Update main.tf

--- infrastructure/dev-env/main.tf
+++ infrastructure/dev-env/main.tf
@@ -1,20 +1,20 @@
 
 module "networking" {
-  source = "./modules/networking"
+  source = "../modules/networking"
 
   project_id = var.project_id
   region = var.region
 }
 
 module "k8s" {
-  source = "./modules/k8s"
+  source = "../modules/k8s"
 
   project_id = var.project_id
   region = var.region
 }
 
 module "account-creator" {
-  source = "./modules/account-mgmt"
+  source = "../modules/account-mgmt"
 
   project_id = var.project_id
   region = var.region
