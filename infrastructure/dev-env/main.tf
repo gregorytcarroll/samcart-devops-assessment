e7bc0637fcc60b976e7b62a7384e51632b395fc1
Gregory Carroll <gregscarroll@gmail.com>
2023-09-10 21:56:08 -0600

    Changed region

--- infrastructure/dev-env/main.tf
+++ infrastructure/dev-env/main.tf
@@ -2,21 +2,18 @@
 module "networking" {
   source = "../modules/networking"
 
-  project_id = var.project_id
   region = var.region
 }
 
 module "k8s" {
   source = "../modules/k8s-infra"
 
-  project_id = var.project_id
   region = var.region
 }
 
 module "account-creator" {
   source = "../modules/account-mgmt"
 
-  project_id = var.project_id
   region = var.region
 }
 
---
+++ infrastructure/dev-env/variables.tf
@@ -0,0 +1,5 @@
+variable "region" {
+    description = "(Required) The AWS region this account will be created in."
+    type        = string
+    default    = "us-west-2"
+}
