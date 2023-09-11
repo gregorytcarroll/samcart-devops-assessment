8599d92e9c2b03658ec47bd028d6ff44fa1da2d2
Gregory Carroll <gregscarroll@gmail.com>
2023-09-10 21:29:07 -0600

    Update main.tf

--- infrastructure/modules/account-mgmt/main.tf
+++ infrastructure/modules/account-mgmt/main.tf
@@ -1,14 +1,29 @@
-module "account-creation" {
-  source  = "tedilabs/account/aws//modules/account"
-  
-  name = "samcart-assessment"
+module "aft" {
+  source = "git@github.com:aws-ia/terraform-aws-control_tower_account_factory.git"
+
+  # Required Parameters
+  ct_management_account_id    = "427071048654"
+  log_archive_account_id      = "427071048654"
+  audit_account_id            = "427071048654"
+  aft_management_account_id   = "427071048654"
+  ct_home_region              = "us-east-1"
+  tf_backend_secondary_region = "us-west-2"
+
+  # Optional Parameters
+  terraform_distribution = "oss"
+  vcs_provider           = "github"
+
+  # Optional Feature Flags
+  aft_feature_delete_default_vpcs_enabled = false
+  aft_feature_cloudtrail_data_events      = false
+  aft_feature_enterprise_support          = false
 }
 
 module "account-setup" {
   source  = "daringway/account-setup/aws"
   version = "3.1.0"
 
-  depends_on = [module.account-creation]
+  depends_on = [module.aft]
 
   cloudtrail_enabled = true
   default_security_group_managed = true
