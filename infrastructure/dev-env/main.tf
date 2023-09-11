02b6542a7cca2edd49780a6652b70e82e5e1f61e
Gregory Carroll <gregscarroll@gmail.com>
2023-09-10 22:15:35 -0600

    Update main.tf

--- infrastructure/dev-env/main.tf
+++ infrastructure/dev-env/main.tf
@@ -5,10 +5,6 @@ module "networking" {
 
 module "k8s" {
   source = "../modules/k8s-infra"
-
-  vpc_id = module.vpc.vpc_id
-  subnet_ids = module.vpc.public_subnets
-  control_plane_subnet_ids = module.vpc.private_subnets
 }
 
 module "account-creator" {
