2a1f18a0891fdc371ab2bb643800be321319d0f3
Gregory Carroll <gregscarroll@gmail.com>
2023-09-11 20:03:10 -0600

    Update main.tf

--- infrastructure/modules/k8s-infra/main.tf
+++ infrastructure/modules/k8s-infra/main.tf
@@ -1,12 +1,10 @@
 module "eks_cluster" {
   source           = "terraform-aws-modules/eks/aws"
-  cluster_name     = "my-eks-cluster"
+  cluster_name     = "dev-cluster"
   cluster_version  = "1.21"
-  subnets          = module.vpc.private_subnets_ids
+  subnet_ids          = module.vpc.private_subnets_ids
   vpc_id           = module.vpc.vpc_id
   tags             = { "Environment" = "Development" }
-
-  workers_additional_security_group_ids = []  # Add any additional security groups needed
 
   fargate_profiles = [
     {
