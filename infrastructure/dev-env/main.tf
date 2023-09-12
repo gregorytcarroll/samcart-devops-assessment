69069db72babcdc2b7c6fc3407d1fbb59776420b
Gregory Carroll <gregscarroll@gmail.com>
2023-09-11 21:34:22 -0600

    Update main.tf

--- infrastructure/dev-env/main.tf
+++ infrastructure/dev-env/main.tf
@@ -3,6 +3,16 @@ module "k8s" {
 
   depends_on = [ module.networking ]
 
-  cluster_name = var.eks_cluster_name
+  eks_cluster_name = var.eks_cluster_name
+  eks_cluster_version = var.eks_cluster_version
+  private_subnets = var.private_subnets
+  public_subnets = var.public_subnets
+  eks_node_desired_capacity = var.eks_node_desired_capacity
+  eks_node_max_capacity = var.eks_node_max_capacity
+  eks_node_min_capacity = var.eks_node_min_capacity
+  eks_node_key_name = var.eks_node_key_name
+  eks_node_instance_type = var.eks_node_instance_type
+  
+
   
 }
