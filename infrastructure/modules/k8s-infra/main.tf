c93ed0b7529df09aefa120d1c05e97a9d3a300fd
Gregory Carroll <gregscarroll@gmail.com>
2023-09-11 20:42:35 -0600

    Update main.tf

--- infrastructure/modules/k8s-infra/main.tf
+++ infrastructure/modules/k8s-infra/main.tf
@@ -3,7 +3,7 @@ module "eks_cluster" {
   
   cluster_name    = "my-eks-cluster"
   cluster_version = "1.21"
-  subnets_ids         = module.networking.private_subnets_ids
+  subnet_ids         = module.networking.private_subnets_ids
   vpc_id          = module.networking.vpc_id
 
   # EKS worker node settings (customize as needed)
