19188a5f56803dfeba61e4eef580598e95aa68b0
Gregory Carroll <gregscarroll@gmail.com>
2023-09-11 21:25:23 -0600

    Update variables.tf

--- infrastructure/dev-env/variables.tf
+++ infrastructure/dev-env/variables.tf
@@ -1,4 +1,5 @@
 # Variables for the EKS cluster module
+
 variable "eks_cluster_name" {
   description = "Name for the EKS cluster"
   type        = string
@@ -40,6 +41,7 @@ variable "eks_node_volume_size" {
 }
 
 # Variables for the VPC module
+
 variable "vpc_name" {
   description = "Name for the VPC"
   type        = string
@@ -88,16 +90,6 @@ variable "manage_default_security_group" {
 variable "default_security_group_name" {
   description = "Name for the default security group"
   type        = string
-}
-
-variable "public_subnet_tags" {
-  description = "Tags for public subnets"
-  type        = map(string)
-}
-
-variable "private_subnet_tags" {
-  description = "Tags for private subnets"
-  type        = map(string)
 }
 
 variable "vpc_tags" {
