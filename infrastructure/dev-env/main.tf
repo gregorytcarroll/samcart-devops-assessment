cb2953fd02cde1b8723dc7997eeb2380f6849f42
Gregory Carroll <gregscarroll@gmail.com>
2023-09-11 19:09:42 -0600

    Update main.tf

--- infrastructure/dev-env/main.tf
+++ infrastructure/dev-env/main.tf
@@ -1,5 +1,16 @@
 module "networking" {
   source = "../modules/networking"
+
+  vpc_name = var.vpc_name
+  vpc_cidr = var.vpc_cidr
+  availability_zones = var.availability_zones
+  private_subnets = var.private_subnets
+  public_subnets = var.public_subnets
+  enable_nat_gateway = var.enable_nat_gateway
+  single_nat_gateway = var.single_nat_gateway
+  enable_dns_hostnames = var.enable_dns_hostnames
+  manage_default_security_group = var.manage_default_security_group
+  default_security_group_name = var.default_security_group_name
 }
 
 module "k8s" {
