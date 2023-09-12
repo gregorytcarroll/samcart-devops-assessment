4913a8d12ad9ffa5e1b4652843dc6cf7e957c720
Gregory Carroll <gregscarroll@gmail.com>
2023-09-11 21:00:54 -0600

    Update outputs.tf

--- infrastructure/modules/k8s-infra/outputs.tf
+++ infrastructure/modules/k8s-infra/outputs.tf
@@ -1,20 +1,4 @@
 
-output "eks_cluster_endpoint" {
-  value = module.eks_cluster.eks_cluster_endpoint
-}
-
-output "eks_cluster_kubeconfig" {
-  value = module.eks_cluster.kubeconfig_filename
-}
-
-output "lb_dns_name" {
-  value = aws_lb.example_lb.dns_name
-}
-
-output "s3_bucket_name" {
-  value = aws_s3_bucket.example_bucket.id
-}
-
 output "vpc_id" {
   description = "The ID of the VPC"
   value       = module.networking.vpc_id
