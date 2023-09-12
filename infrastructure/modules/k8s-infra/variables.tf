7cea7dcbaee632e85d80d57d484ba6b723a622ed
Gregory Carroll <gregscarroll@gmail.com>
2023-09-11 19:27:32 -0600

    Change

--- infrastructure/modules/k8s-infra/variables.tf
+++ infrastructure/modules/k8s-infra/variables.tf
@@ -9,3 +9,11 @@ variable "vpc_id" {
 variable "subnet_ids" {
     type = list(string)
 }
+
+variable "iam_policy_prefixes" {
+  type    = list(string)
+  default = [
+    "${local.iam_role_policy_prefix}/AmazonEKSWorkerNodePolicy",
+    "${local.iam_role_policy_prefix}/AmazonEC2ContainerRegistryReadOnly",
+  ]
+}
