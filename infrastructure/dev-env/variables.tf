35afe02a69b921c1d62a42c50b108f9a2903360e
Gregory Carroll <gregscarroll@gmail.com>
2023-09-11 19:11:00 -0600

    Update variables.tf

--- infrastructure/dev-env/variables.tf
+++ infrastructure/dev-env/variables.tf
@@ -8,3 +8,55 @@
 variable "subnet_ids" {
     type = list(string)
 }
+
+## Networking ##
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "List of private subnets CIDR blocks"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "List of public subnets CIDR blocks"
+  type        = list(string)
+}
+
+variable "enable_nat_gateway" {
+  description = "Enable NAT gateway"
+  type        = bool
+}
+
+variable "single_nat_gateway" {
+  description = "Use a single NAT gateway for all private subnets"
+  type        = bool
+}
+
+variable "enable_dns_hostnames" {
+  description = "Enable DNS hostnames"
+  type        = bool
+}
+
+variable "manage_default_security_group" {
+  description = "Manage the default security group for the VPC"
+  type        = bool
+}
+
+variable "default_security_group_name" {
+  description = "Name of the default security group"
+  type        = string
+}
