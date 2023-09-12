ecd76e3d6594e2286e7c3828e0afac4568db8a43
Gregory Carroll <gregscarroll@gmail.com>
2023-09-11 21:13:42 -0600

    Update variables.tf

--- infrastructure/modules/k8s-infra/variables.tf
+++ infrastructure/modules/k8s-infra/variables.tf
@@ -1,3 +1,12 @@
- variable "cluster_name" {
+variable "cluster_name" {
      type = string
+     default = "dev-env-cluster"
+ }
+
+variable "vpc_id" {
+     type = string
+ }
+   
+variable "subnet_ids" {
+        type = list(string)
  }
