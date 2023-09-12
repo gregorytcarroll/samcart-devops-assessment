723def41fda484adb5d9eb30054634203c8461e2
Gregory Carroll <gregscarroll@gmail.com>
2023-09-11 19:59:57 -0600

    Update main.tf

--- infrastructure/modules/k8s-infra/main.tf
+++ infrastructure/modules/k8s-infra/main.tf
@@ -1,77 +1,19 @@
+module "eks_cluster" {
+  source           = "terraform-aws-modules/eks/aws"
+  cluster_name     = "my-eks-cluster"
+  cluster_version  = "1.21"
+  subnets          = module.vpc.private_subnets_ids
+  vpc_id           = module.vpc.vpc_id
+  tags             = { "Environment" = "Development" }
 
-module "eks" {
-  source = "../.."
+  workers_additional_security_group_ids = []  # Add any additional security groups needed
 
-  cluster_name                   = var.cluster_name
-  cluster_version                = "1.27"
-  cluster_endpoint_public_access = true
-
-  cluster_addons = {
-    kube-proxy = {}
-    vpc-cni    = {}
-    coredns = {
-      configuration_values = jsonencode({
-        computeType = "Fargate"
-      })
-    }
-  }
-
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.public_subnets
-
-  # Fargate profiles use the cluster primary security group so these are not utilized
-  create_cluster_security_group = false
-  create_node_security_group    = false
-
-  fargate_profile_defaults = {
-    iam_role_additional_policies = {
-      additional = aws_iam_policy.additional.arn
-    }
-  }
-
-  fargate_profiles = merge(
+  fargate_profiles = [
     {
-      example = {
-        name = "dev"
-        selectors = [
-          {
-            namespace = "backend"
-            labels = {
-              Application = "backend"
-            }
-          },
-          {
-            namespace = "app-*"
-            labels = {
-              Application = "app-wildcard"
-            }
-          }
-        ]
-
-        # Using specific subnets instead of the subnets supplied for the cluster itself
-        subnet_ids = [module.vpc.private_subnets[1]]
-
-        tags = {
-          Owner = "secondary"
-        }
-
-        timeouts = {
-          create = "20m"
-          delete = "20m"
-        }
-      }
-    },
-    { for i in range(3) :
-      "kube-system-${element(split("-", local.azs[i]), 2)}" => {
-        selectors = [
-          { namespace = "kube-system" }
-        ]
-        # We want to create a profile per AZ for high availability
-        subnet_ids = [element(module.vpc.private_subnets, i)]
-      }
+      name            = "dev"
+      pod_execution_role_arn = aws_iam_role.eks_node_role.arn
+      subnets_ids     = module.vpc.private_subnets_ids
+      tags = { "Environment" = "Development" }
     }
-  )
-
-  tags = local.tags
+  ]
 }
