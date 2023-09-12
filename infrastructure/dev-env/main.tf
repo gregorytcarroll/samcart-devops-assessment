3bc0eac6194c172121cd6d17535e5dc4e98c05fd
Gregory Carroll <gregscarroll@gmail.com>
2023-09-11 20:43:40 -0600

    Update main.tf

--- infrastructure/dev-env/main.tf
+++ infrastructure/dev-env/main.tf
@@ -17,9 +17,7 @@ module "k8s" {
   source = "../modules/k8s-infra"
 
   depends_on = [ module.networking ]
-
-  vpc_id = module.networking.vpc_id
-  subnet_ids = var.subnet_ids
+  
   cluster_name = var.cluster_name
 }
 
