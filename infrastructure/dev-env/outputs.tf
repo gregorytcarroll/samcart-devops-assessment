c926733f3165ae4054389fbddffd4356e818665b
Gregory Carroll <gregscarroll@gmail.com>
2023-09-11 20:54:49 -0600

    Update outputs.tf

--- infrastructure/dev-env/outputs.tf
+++ infrastructure/dev-env/outputs.tf
@@ -1,8 +1,4 @@
-# output "vpc_id" {
-#     value = module.networking.vpc_id
-# }
-
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       = module.vpc.vpc_id
+  value       = module.networking.vpc_id
 }
